326e61b6cbaaa38e62b4d6b66af32f540995794f
hamzaboularbah <boularbah59@gmail.com>
2020-04-18 14:30:20 +0100

    Updates

---
+++ precache-manifest.8ef0466580924c1efcc43eb3fa276a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64681403f1c560f8f6957fa463048d7",
+    "url": "/react-resize-tracker/index.html"
+  },
+  {
+    "revision": "e575c80efe5f47740758",
+    "url": "/react-resize-tracker/static/css/main.2d8bd839.chunk.css"
+  },
+  {
+    "revision": "56401e7489308d96b31c",
+    "url": "/react-resize-tracker/static/js/2.93e9d5f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-resize-tracker/static/js/2.93e9d5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e575c80efe5f47740758",
+    "url": "/react-resize-tracker/static/js/main.028b83e5.chunk.js"
+  },
+  {
+    "revision": "f821817ce2e832edebd3",
+    "url": "/react-resize-tracker/static/js/runtime-main.7a8402d4.js"
+  }
+]);
