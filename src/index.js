afe0c68b0f575ab8238d532b4a108443d822debc
Hamza Boularbah <boularbah59@gmail.com>
2020-05-22 12:05:50 +0000

    modified useLayoutEffect

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useWindowResize = (_) => {
   const [size, setSize] = useState([0, 0])
-  useLayoutEffect((_) => {
-    const updateSize = (_) => {
+  useEffect((_) => {
+    const updateSize = () => {
       setSize([window.innerWidth, window.innerHeight])
     }
     window.addEventListener('resize', updateSize)
     updateSize()
-    return (_) => window.removeEventListener('resize', updateSize)
+    return () => window.removeEventListener('resize', updateSize)
   }, [])
   return size
 }
